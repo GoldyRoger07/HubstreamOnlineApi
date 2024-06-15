030ec5ef9c4b8947af91391219c2f704af4e426d
Latouche Gerson <latouche822@gmail.com>
2024-06-15 01:32:16 -0400

    one of last commit

--- src/main/resources/static/script.sql
+++ src/main/resources/static/script.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE hubstream_online_api;
 
 INSERT INTO plans(id_plan,duree,duree_extension,prix,titre,type,points,livraison) VALUES
-(0,1,'Jour',50,'All-J1','All',9,0),
-(0,3,'Jours',125,'All-J3','All',12,1),
-(0,7,'Jours',295,'All-J7','All',17,2),
-(0,30,'Jours',1150,'All-J30','All',24,8);
+(0,1,'Jour',50,'All-J1','All',5,0),
+(0,3,'Jours',125,'All-J3','All',15,1),
+(0,7,'Jours',295,'All-J7','All',35,2),
+(0,30,'Jours',1150,'All-J30','All',150,8);
 
 
 UPDATE plans set livraison=0 WHERE id_plan=1;
